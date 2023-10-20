71d95b83d30f644d734aaddecb50766e4010405a
ajsaenz1091 <ajsaenz1091@gmail.com>
2023-10-20 09:00:37 -0600

    create user authentication component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
       <Route path='/' element={  <Navigation /> }>
        <Route index element={  <Home /> }/>
        {/* <Route index element={  <Shop /> }/> */}
-       <Route path='/sign-in' element={  <SignIn /> }/>
+       <Route path='auth' element={  <Authentication /> }/>
       </Route>
     </Routes>
   );
