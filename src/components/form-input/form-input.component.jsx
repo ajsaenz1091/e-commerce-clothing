fac0c901024fa5c85b882cda0d8b1e45e55dcbd6
ajsaenz1091 <ajsaenz1091@gmail.com>
2023-10-23 19:38:06 -0600

    more styled components refactors and abstrcated logic for directory items

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,19 +1,20 @@
-import './form-input.styles.scss';
+import { Group, FromInputLabel, Input } from "./form-input.styles";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className='group'>
-      <input className='form-input' {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label
+        <fromInputLabel
+          shrink={otherProps.value.length}
           className={`${
             otherProps.value.length ? 'shrink' : ''
           } form-input-label`}
         >
           {label}
-        </label>
+        </fromInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
